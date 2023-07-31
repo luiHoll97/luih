ddc6cd747aff6329f2dd5b95961097e6bb8007db
luiHoll97 <tech.luiholl@gmail.com>
2023-07-31 15:46:08 +0100

    inital commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,8 +3,4 @@ import { screen } from "@testing-library/react"
 import { render } from "./test-utils"
 import { App } from "./App"
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn chakra/i)
-  expect(linkElement).toBeInTheDocument()
-})
+
