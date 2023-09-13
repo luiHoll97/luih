a617d26390b41dbdf23d8206647f92277049fbd4
luiHoll97 <tech.luiholl@gmail.com>
2023-09-13 16:43:46 +0100

    added techs for badge colours

--- src/utils/sortBadges.ts
+++ src/utils/sortBadges.ts
@@ -1,27 +1,35 @@
 export const sortBadges = (tech: string): string => {
-    switch (tech) {
-        case 'react':
-            return 'blue'
-        case 'firebase':
-            return 'orange'
-        case 'redux':
-            return 'purple'
-        case 'chakra-ui':
-            return 'teal'
-        case 'typeScript':
-            return 'cyan'
-        case 'API':
-            return 'green'
-        case 'Sass':
-            return 'pink'
-        case 'express':
-            return 'yellow'
-        case 'PostgreSQL':
-            return 'red'
-        case 'node':
-            return 'green'
-            
-        default:
-            return 'gray'
-    }
-}
+  switch (tech) {
+    case "React":
+      return "blue";
+    case "Firebase":
+      return "orange";
+    case "Redux":
+      return "purple";
+    case "Chakra-ui":
+      return "teal";
+    case "TypeScript":
+      return "cyan";
+    case "API":
+      return "green";
+    case "Sass":
+      return "pink";
+    case "Express":
+      return "yellow";
+    case "PostgreSQL":
+      return "red";
+    case "Node":
+      return "green";
+    case "Immer":
+      return "blue";
+    case "MobX":
+      return "green";
+    case "JavaScript":
+      return "yellow";
+    case "CSS":
+      return "pink";
+
+    default:
+      return "gray";
+  }
+};
