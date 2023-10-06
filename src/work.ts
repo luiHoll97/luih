91ef5eb3d500394cd4f2d45e09bb5b5bd812892f
luiHoll97 <tech.luiholl@gmail.com>
2023-10-06 17:49:43 +0100

    work addition

--- src/work.ts
+++ src/work.ts
@@ -4,11 +4,86 @@ import {
   FaBook,
   FaTicketAlt,
   FaCheckDouble,
+  FaSignal,
 } from "react-icons/fa";
 import { BiCricketBall, BiSolidCricketBall } from "react-icons/bi";
 import { Project } from "./types/project";
 
 export const Projects: Project[] = [
+  {
+    id: "12",
+    title: "php-api",
+    description:
+      "An intro to PHP/Laravel. The project is a simple server to get & post data to a CSV file, info on how to run is on the code link",
+    status: true,
+    techs: ["PHP", "API", "Laravel"],
+    link: "",
+    image: FaSignal,
+    team: false,
+    docuemntation:
+      "https://lui-holliday-docs.notion.site/php-API-74062e43286b44978a930bf9e4746853?pvs=4",
+    code: "https://github.com/luiHoll97/api-php",
+    linksAvailable: true,
+  },
+
+  {
+    id: "9",
+    title: "immer todo",
+    description:
+      "A simple todo app using immer. I am also going through the immer docs and making notes (see my docs).",
+    image: FaTicketAlt,
+    techs: ["React", "Immer", "Chakra-ui"],
+    link: "https://immer-todo.netlify.app",
+    status: true,
+    team: false,
+    docuemntation:
+      "https://lui-holliday-docs.notion.site/immer-docs-31d4d8fad9534960b4f19ad62ffc824b?pvs=4",
+    code: "https://github.com/luiHoll97/immer-todo",
+    linksAvailable: true,
+  },
+  {
+    id: "11",
+    title: "rollup redux",
+    description:
+      "A project I built during a tech test. 20mins to create a simple redux app",
+    image: FaCheckDouble,
+    techs: ["React", "Redux", "CSS"],
+    link: "",
+    status: true,
+    team: false,
+    docuemntation: "",
+    code: "https://github.com/luiHoll97/rollup-redux",
+    linksAvailable: true,
+  },
+  {
+    id: "8",
+    title: "redux scorer",
+    description:
+      "A simple cricket scoreboard app using redux. I used this to see how I could use redux to manage state for my other app.",
+    image: BiSolidCricketBall,
+    techs: ["React", "Redux"],
+    link: "https://redux-scorer.netlify.app",
+    status: false,
+    team: false,
+    docuemntation: "",
+    code: "https://github.com/luiHoll97/redux-scorer",
+    linksAvailable: true,
+  },
+  {
+    id: "10",
+    title: "mobx todo",
+    description:
+      "Todo app using mobX and redux (for comparison purposes). This uses the immer-todo CSS template",
+    image: FaCheckDouble,
+    techs: ["React", "MobX", "Chakra-ui"],
+    link: "https://luiholl-mobx-todo.netlify.app",
+    status: true,
+    team: false,
+    docuemntation:
+      "https://lui-holliday-docs.notion.site/mobx-todo-bc62262b194b4ec0b80cda36d26bb05c?pvs=4",
+    code: "https://github.com/luiHoll97/mobx-todo",
+    linksAvailable: true,
+  },
   {
     id: "4",
     title: "Portfolio",
@@ -35,65 +110,6 @@ export const Projects: Project[] = [
     team: true,
     docuemntation: "",
     code: "https://github.com/maemastersdev/C5C2-Study-Resources-Backend/tree/main",
-    linksAvailable: true,
-  },
-  {
-    id: "8",
-    title: "redux scorer",
-    description:
-      "A simple cricket scoreboard app using redux. I used this to see how I could use redux to manage state for my other app.",
-    image: BiSolidCricketBall,
-    techs: ["React", "Redux"],
-    link: "https://redux-scorer.netlify.app",
-    status: false,
-    team: false,
-    docuemntation: "",
-    code: "https://github.com/luiHoll97/redux-scorer",
-    linksAvailable: true,
-  },
-  {
-    id: "9",
-    title: "immer todo",
-    description:
-      "A simple todo app using immer. I am also going through the immer docs and making notes (see my docs).",
-    image: FaTicketAlt,
-    techs: ["React", "Immer", "Chakra-ui"],
-    link: "https://immer-todo.netlify.app",
-    status: true,
-    team: false,
-    docuemntation:
-      "https://lui-holliday-docs.notion.site/immer-docs-31d4d8fad9534960b4f19ad62ffc824b?pvs=4",
-    code: "https://github.com/luiHoll97/immer-todo",
-    linksAvailable: true,
-  },
-  {
-    id: "10",
-    title: "mobx todo",
-    description:
-      "Todo app using mobX and redux (for comparison purposes). This uses the immer-todo CSS template",
-    image: FaCheckDouble,
-    techs: ["React", "MobX", "Chakra-ui"],
-    link: "https://luiholl-mobx-todo.netlify.app",
-    status: true,
-    team: false,
-    docuemntation:
-      "https://lui-holliday-docs.notion.site/mobx-todo-bc62262b194b4ec0b80cda36d26bb05c?pvs=4",
-    code: "https://github.com/luiHoll97/mobx-todo",
-    linksAvailable: true,
-  },
-
-  {
-    id: "11",
-    title: "rollup redux",
-    description:
-      "A project I built during a tech test. 20mins to create a simple redux app",
-    image: FaCheckDouble,
-    techs: ["React", "Redux", "CSS"],
-    link: "",
-    status: true,
-    team: false,
-    docuemntation: "",
-    code: "https://github.com/luiHoll97/rollup-redux",
     linksAvailable: true,
   },
   {
